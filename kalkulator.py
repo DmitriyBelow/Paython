e5b27cdd9fdb7a804606ea7d018a5d197e1194ed
DmitriyBelow <99124419+DmitriyBelow@users.noreply.github.com>
2022-02-06 16:07:09 +0300

    калькулятор на пайтон V1.2

--- kalkulator.py
+++ kalkulator.py
@@ -2,56 +2,19 @@ import math
 from colorama import init
 init()
 from colorama import Fore, Back, Style
-
-print(Fore.RED)
-do = input(" действие: ")
- 
- # арифметические действия
-
-def plus(a, b):
-    c = a + b
-    print(c)
-
-def minus(a, b):
-    c = a - b
-    print(c)
-
-def mult(a, b):
-    c = a * b
-    print(c)
-
-def div(a, b):
-    c = a / b 
-    print(c)
-
-def degree(a,b):
-    c = a ** b
-    print(c)
-
-if do == "pi":
-    print(Fore.YELLOW)
+# основная часть кода
+# ввод примера для вычислительных действаий
+print(Fore.CYAN)
+example = input('Действие: ')
+# определение действия для подщёта
+if example == "pi":
+    print(Fore.GREEN)
     print(math.pi)
-elif do != "pi":
-    print(Fore.CYAN)
-    x = float(input(" первое число: "))
-    y = float(input(" второе число: "))
-    if do == "+":  # действие"+"
-        print(Fore.YELLOW)
-        plus(x, y)
-    elif do == "-":  # действие"-"
-        print(Fore.YELLOW)
-        minus(x, y)
-    elif do == "*":  # дейтвие"*"
-        print(Fore.YELLOW)
-        mult(x, y)
-    elif do == "/":  # действие"/"
-        print(Fore.YELLOW)
-        div(x, y)
-    elif do == "**":  # возведение числа в степень
-        print(Fore.YELLOW)
-        degree(x, y )
-    else:
-        print(Fore.BLACK)
-        print(" неверное действие, повторите попытку!")
+elif example != "pi":
+    print(Fore.GREEN)
+    print(example + ' = ' + str(eval(example)))
+# добавление примера в общий список истори
+exas = []
+exas.append(example + ' = ' + str(eval(example)))
 
 input() 
